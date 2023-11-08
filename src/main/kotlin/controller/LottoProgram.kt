17f1e3dbe565797f64cd78f3b30569f4b8f40620
uch0731 <uch0731@naver.com>
2023-11-08 19:31:08 +0900

    선택사항 5번 돈이 떨어질때까지 로또 무한루프

--- src/main/kotlin/controller/LottoProgram.kt
+++ src/main/kotlin/controller/LottoProgram.kt
@@ -5,24 +5,34 @@ import service.LottoService
 import view.SystemView
 
 class LottoProgram {
-    val lotto = Lotto()
-    val lottoService: LottoService
-    val systemView: SystemView
-
-    init {
-        lottoService = LottoService()
-        systemView = SystemView()
-    }
+    val lotto: Lotto = Lotto(IntArray(6), 0, 0, mutableListOf<IntArray>(), 0, IntArray(4))
+    val lottoService: LottoService = LottoService()
+    val systemView: SystemView = SystemView()
 
     fun run() {
-        systemView.showStartMessage()
-        systemView.showManualLottoBuyMessage(lottoService.canBuyMax())
-        lottoService.setManualCountAndAutoCount(lotto)
+        start()
+        selectLottoNum()
+        end()
+        replay()
+    }
 
+    fun start() {
+        systemView.showStartMessage()
+    }
+
+    fun selectLottoNum() {
+        systemView.showManualLottoBuyMessage()
+        lottoService.setManualCountAndAutoCount(lotto, systemView.totalCount)
+        println(lotto.manualLottoCount)
+        println(lotto.autoLottoCount)
+        println(systemView.totalCount)
+
+        lotto.lottoNum.clear()
         for (i in 0 until lotto.manualLottoCount) {
             systemView.showNumInputMessage(i)
             lottoService.setManualLottoNum(lotto)
         }
+
         lotto.winNum = lottoService.makeRandomNum()
         systemView.showStartAutoLottoMessage()
 
@@ -30,9 +40,19 @@ class LottoProgram {
             lotto.lottoNum.add(lottoService.makeRandomNum())
         }
         systemView.showFinishAutoLottoMessage()
+    }
+
+    fun end() {
         systemView.showLottoNumMessage()
         lottoService.showLottoNum(lotto)
         systemView.showWinNumMessage(lotto.winNum)
         systemView.showLottoResultMessage(lottoService.getResult(lotto))
+    }
+
+    fun replay() {
+        if (lotto.prize >= 1000) {
+            systemView.showRestartLottoMessage(lotto)
+            run()
+        }
     }
 }
