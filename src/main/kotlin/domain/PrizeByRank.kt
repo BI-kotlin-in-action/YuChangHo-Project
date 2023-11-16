e86991af1098740abc4e985f132ba390159aa471
uch0731 <uch0731@naver.com>
2023-11-16 12:38:12 +0900

    PrizeByRank 로또 맞은개수 변수추가

--- src/main/kotlin/domain/PrizeByRank.kt
+++ src/main/kotlin/domain/PrizeByRank.kt
@@ -1,18 +1,16 @@
 package domain
 
-import service.LottoService
-
-enum class PrizeByRank(val rank: Int, val prize: Int) {
-    FIRST(1, 100000),
-    SECOND(2, 5000),
-    THIRD(3, 100),
-    FOURTH(4, 5),
-    LOSE(5, 0), ;
+enum class PrizeByRank(val rank: Int, val correctCount: Int, val prize: Int) {
+    FIRST(1, 6, 100000),
+    SECOND(2, 5, 5000),
+    THIRD(3, 4, 100),
+    FOURTH(4, 3, 5),
+    LOSE(5, 2, 0), ;
 
     companion object {
         @JvmStatic
         fun getRank(count: Int): PrizeByRank {
-            return values().find { it.rank == LottoService.LOTTO_SIZE - count + 1 } ?: LOSE
+            return values().find { it.correctCount == count } ?: LOSE
         }
     }
 }
