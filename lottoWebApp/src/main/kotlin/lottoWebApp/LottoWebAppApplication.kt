d2e1c2074dabbcd121ed30df7bccaf2d2a833ebc
uch0731 <uch0731@naver.com>
2023-11-28 14:19:45 +0900

    test

---
+++ lottoWebApp/src/main/kotlin/lottoWebApp/LottoWebAppApplication.kt
@@ -0,0 +1,11 @@
+package lottoWebApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LottoWebAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<LottoWebAppApplication>(*args)
+}
